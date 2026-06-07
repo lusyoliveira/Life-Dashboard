2634c3a9c254270c1715c5af268c10228ecabf7f
Luciene Oliveira <Luciene Oliveira@CASA2>
2026-06-07 13:05:39 -0300

    Implementada recorrencia na agenda,

--- js/modulos/contas/ContasViewModel.js
+++ js/modulos/contas/ContasViewModel.js
@@ -69,6 +69,7 @@ export class ContasViewModel {
         t.ContaOrigem.id === contaId ||
         t.ContaDestino?.id === contaId
     );
+console.log(transacoesConta);
 
     const somaMovimentacoes = transacoesConta.reduce((acc, t) => {
         return acc + Number(t.Valor);
